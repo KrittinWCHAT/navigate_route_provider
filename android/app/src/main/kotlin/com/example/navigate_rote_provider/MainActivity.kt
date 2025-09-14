373709313d59e012e14c9cf88fd5e170a87bb816
KrittinWCHAT <gsbthus@gmail.com>
2025-09-14 09:56:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/navigate_rote_provider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.navigate_rote_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'screens/detail_screen.dart';
+import 'screens/home_screen.dart';
+import 'package:navigate_rote_provider/provider/counter_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        DetailScreen.routeName: (context) => const DetailScreen(),
+      },
+    );
+  }
+}
